b84e7ece07053c2f462b7e77ef88e5b40e1ff963
Логист <@LOGIST>
2018-03-05 17:36:00 +0200

    git remove ConsoleApplication1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-            Console.WriteLine("git remove ConsoleApplication1");
+            Console.WriteLine("git remove ConsoleApplication1");//ывываываы
             Console.WriteLine("Hello world");
         }
     }
